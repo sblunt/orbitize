97dab717d7a568f640bf8895e183a89b3be7cc2d
Jason Wang <jason.wang@outlook.com>
2018-01-12 12:35:45 -0800

    we have  a very basic likelihood

---
+++ lnlike.py
@@ -0,0 +1,14 @@
+def chi2_likelihood(data, errors, model):
+    """
+    Log of the Chi2 Likelihood Computation
+
+    Args:
+        data (np.array): 1-D array of data
+        errors (np.array): 1-D array of errors for each data point
+        model (np.array): 1-D array of model predictions
+    """
+
+    chi2 = (data - model)**2/errors**2
+
+    return chi2
+
