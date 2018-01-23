78f01194fa2ededa3bb98b05710737789196e7ea
Henry Ngo <hhkngo@gmail.com>
2018-01-22 22:11:51 -0800

    Renamed `read_csv` to `read_formatted_file` to reflect the flexibility of `astropy.io.ascii.read()`.
    Updated docstring
    
    Updated test for this function

--- tests/test_input.py
+++ tests/test_input.py
@@ -2,9 +2,9 @@ import os
 from orbitize.read_input import read_csv
 
 
-def test_read_csv():
+def test_read_formatted_file():
     """
-    Test the read_csv function using the test_val.csv file
+    Test the read_formatted_file function using the test_val.csv file
     """
     testdir = os.path.dirname(os.path.abspath(__file__))
     input_file = os.path.join(testdir, 'test_val.csv')
