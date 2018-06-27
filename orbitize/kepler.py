81d48cc3a67d30e965844c391242da0e8c3745be
sblunt <sarah.blunt.3@gmail.com>
2018-06-27 11:22:26 -0700

    Added np.squeeze command back in for vz
    kepler.py was failing for scalar values; fixed.

--- orbitize/kepler.py
+++ orbitize/kepler.py
@@ -28,9 +28,9 @@ def calc_orbit(epochs, sma, ecc, tau, argp, lan, inc, plx, mtot, mass=None, tole
         max_iter (int, optional): maximum number of iterations before switching. Defaults to 100.
 
     Return:
-        raoff (np.array): 2-D array (n_dates x n_orbs) of RA offsets between the bodies (origin is at the other body)
-        deoff (np.array): 2-D array (n_dates x n_orbs) of Dec offsets between the bodies
-        vz (np.array): 2-D array (n_dates x n_orbs) of radial velocity offset between the bodies
+        raoff (np.array): array-like (n_dates x n_orbs) of RA offsets between the bodies (origin is at the other body)
+        deoff (np.array): array-like (n_dates x n_orbs) of Dec offsets between the bodies
+        vz (np.array): array-like (n_dates x n_orbs) of radial velocity offset between the bodies
 
     Written: Jason Wang, Henry Ngo, 2018
     """
@@ -96,6 +96,7 @@ def calc_orbit(epochs, sma, ecc, tau, argp, lan, inc, plx, mtot, mass=None, tole
     # raoff = np.transpose(np.squeeze(raoff)[()])
     # deoff = np.transpose(np.squeeze(deoff)[()])
     # vz = np.transpose(np.squeeze(vz)[()])
+    vz = np.squeeze(vz)[()]
 
     return raoff, deoff, vz
 
