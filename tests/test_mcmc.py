be98be9b098fefffc199b172537c0d8205f03023
Henry Ngo <hhkngo@gmail.com>
2018-07-24 21:05:39 -0700

    more clear argument names in test code

--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -19,7 +19,9 @@ def test_pt_mcmc_runs(num_threads=1):
     orbit = system.System(1, data_table, 1, 0.01)
 
     # construct sampler
-    mcmc = sampler.PTMCMC(chi2_lnlike, orbit, 2, 100, num_threads=num_threads)
+    n_temps=2
+    n_walkers=100
+    mcmc = sampler.PTMCMC(chi2_lnlike, orbit, n_temps, n_walkers, num_threads=num_threads)
 
     # run it a little
     emcee_sampler_obj = mcmc.run_sampler(10, 1)
@@ -41,7 +43,8 @@ def test_ensemble_mcmc_runs(num_threads=1):
     orbit = system.System(1, data_table, 1, 0.01)
 
     # construct sampler
-    mcmc = sampler.EnsembleMCMC(chi2_lnlike, orbit, 100, num_threads=num_threads)
+    n_walkers=100
+    mcmc = sampler.EnsembleMCMC(chi2_lnlike, orbit, n_walkers, num_threads=num_threads)
 
     # run it a little
     emcee_sampler_obj = mcmc.run_sampler(10, burn_steps=1)
