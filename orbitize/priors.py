c01d65b464bb15c0149bc96a23020660cfa31da3
Jason Wang <jason.wang@outlook.com>
2018-08-14 10:17:08 -0700

    fix linear lnprob computation

--- orbitize/priors.py
+++ orbitize/priors.py
@@ -259,11 +259,11 @@ class LinearPrior(Prior):
 
         prob = (self.m*element_array + self.b)/normalizer
 
-        print(prob)
-
         prob[(element_array>x_intercept) | (element_array<0)] = 0.
 
-        return prob
+        lnprob = np.log(prob)
+
+        return lnprob
 
 
 
