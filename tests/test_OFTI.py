52d14e01c822b5f74f81014b6cce6a7297f211a4
isabelangelo <isabelangelo@berkeley.edu>
2018-08-21 12:50:39 -0700

    added test_OFTI.py

--- tests/test_OFTI.py
+++ tests/test_OFTI.py
@@ -8,12 +8,15 @@ import orbitize.driver
 
 def test_scale_and_rotate():
     
-    myDriver = orbitize.driver.Driver('gsc6214_astrometry.csv', 'OFTI',
-    1, 0.8, 9.19,mass_err=0.1, plx_err=0.043)
+#     myDriver = orbitize.driver.Driver('gsc6214_astrometry.csv', 'OFTI',
+#     1, 0.8, 9.19,mass_err=0.1, plx_err=0.043)
+    myDriver = orbitize.driver.Driver('GJ504.csv', 'OFTI',
+    1, 1.22, 56.95,mass_err=0.08, plx_err=0.26)
     
     s = myDriver.sampler
     samples = s.prepare_samples(100)
     
+    #these have been moved to init
     epochs = np.array(s.tbl['epoch']) # may move to init
     sma,ecc,argp,lan,inc,tau,mtot,plx = [s for s in samples]
     epoch_idx = np.argmin(s.sep_err) # may move to init
@@ -25,10 +28,35 @@ def test_scale_and_rotate():
     assert sep_sar == pytest.approx(s.tbl[epoch_idx]['quant1'], abs=s.tbl[epoch_idx]['quant1_err'])
     assert pa_sar == pytest.approx(s.tbl[epoch_idx]['quant2'], abs=s.tbl[epoch_idx]['quant2_err'])
     
-#def test_reject()
+def test_run_sampler():
 
-#def test_terminal_commands()
+#     myDriver = orbitize.driver.Driver('gsc6214_astrometry.csv', 'OFTI',
+#     1, 0.8, 9.19,mass_err=0.1, plx_err=0.043)
+    myDriver = orbitize.driver.Driver('GJ504.csv', 'OFTI',
+    1, 1.22, 56.95,mass_err=0.08, plx_err=0.26)
+    
+    s = myDriver.sampler
+    
+    #test num_samples=1
+    s.run_sampler(0,num_samples=1)
+    
+    #test to make sure outputs are reasonable
+    #look at posteriors in paper, hard code expected values , within 20%?
+    #test which uncertainty seems reasonable
+    orbits = s.run_sampler(100)
+    sma = np.median([x[0] for x in orbits]) # should we use s.system.labels for idx??
+    ecc = np.median([x[1] for x in orbits])
+    inc = np.median([x[4] for x in orbits])
+    
+        
+    #test with only one epoch
+    myDriver = orbitize.driver.Driver('gsc6214_1epoch.csv', 'OFTI',
+    1, 0.8, 9.19,mass_err=0.1, plx_err=0.043)
+    s = myDriver.sampler
+    s.run_sampler(1)
+    
 
 if __name__ == "__main__":
     test_scale_and_rotate()
+    test_run_sampler()
     print("Done!")
