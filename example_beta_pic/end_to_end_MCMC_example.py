1b9246edbd87736b1aa369b3cb3d11969048a6a2
Jason Wang <jwang@astro.berkeley.edu>
2018-10-02 10:19:08 -0700

    Some modifications to beta pic test code

--- example_beta_pic/end_to_end_MCMC_example.py
+++ example_beta_pic/end_to_end_MCMC_example.py
@@ -1,6 +1,7 @@
 # End to end example with beta Pic astrometry
 # Based on driver.py
 
+import numpy as np
 from orbitize import read_input, system, sampler, priors
 import multiprocessing as mp
 
@@ -14,14 +15,21 @@ plx_err=0.12 #mas
 
 # Sampler parameters
 likelihood_func_name='chi2_lnlike'
-n_temps=5
+n_temps=20
 n_walkers=1000
 n_threads=mp.cpu_count()
-total_orbits=1000 # n_walkers x num_steps_per_walker
-burn_steps=1000
+total_orbits=10000 # n_walkers x num_steps_per_walker
+burn_steps=1
 
 # Read in data
 data_table = read_input.read_formatted_file(datafile)
+# convert to mas
+data_table['quant1'] *= 1000
+data_table['quant1_err'] *= 1000
+data_table['quant2'] *= 1000
+data_table['quant2_err'] *= 1000
+
+data_table['epoch'] -= 50000
 
 # Initialize System object which stores data & sets priors
 bP_system = system.System(
@@ -31,14 +39,13 @@ bP_system = system.System(
 
 # We could overwrite any priors we want to here.
 # Using defaults for now.
-#bP_system.sys_priors[3] = priors.UniformPrior(0, np.pi/3)
+bP_system.sys_priors[3] = priors.UniformPrior(np.pi/10, np.pi/2)
 
 # Initialize Sampler object, which stores information about
 # the likelihood function & the algorithm used to generate
 # orbits, and has System object as an attribute.
 bP_sampler = sampler.PTMCMC(likelihood_func_name,bP_system,n_temps,n_walkers,n_threads)
 
-import pdb; pdb.set_trace()
 # Run the sampler to compute some orbits, yeah!
 # Results stored in bP_sampler.chain and bP_sampler.lnlikes
 bP_sampler.run_sampler(total_orbits, burn_steps=burn_steps, thin=10)
