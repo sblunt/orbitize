676cf3f718fa79db06a624bae08f1f3ce610ac7e
Jason Wang <jason.wang@outlook.com>
2019-07-08 11:39:02 -0700

    started making some test code for multiplanet based on hr 8799

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,11 +1,12 @@
 """
 Tests functionality of methods in system.py
 """
+import os
+import numpy as np
 
 import orbitize.read_input as read_input
 import orbitize.system as system
 import orbitize.results as results
-import os
 
 def test_add_and_clear_results():
     num_secondary_bodies=1
@@ -36,5 +37,32 @@ def test_add_and_clear_results():
     test_system.add_results(test_results)
     assert len(test_system.results)==1
 
+def test_multi_planets():
+    """
+    Test using some data for HR 8799 b and c
+    """
+    num_secondary_bodies=2
+    testdir = os.path.dirname(os.path.abspath(__file__))
+    input_file = os.path.join(testdir, 'test_val_multi.csv')
+    data_table=read_input.read_file(input_file)
+    system_mass=1.47
+    plx=24.30
+    mass_err=0.11
+    plx_err=0.7
+    # Initialize System object, use mjd=50000 to be consistent with Wang+2018
+    test_system = system.System(
+        num_secondary_bodies, data_table, system_mass,
+        plx, mass_err=mass_err, plx_err=plx_err, tau_ref_epoch=50000
+    )
+
+    params = np.array([7.2774010e+01, 4.1116819e-02, 5.6322372e-01, 3.5251172e+00, 4.2904768e+00, 9.4234377e-02, 
+              4.5418411e+01, 1.4317369e-03, 5.6322372e-01, 3.1016846e+00, 4.2904768e+00, 3.4033456e-01,
+              2.4589758e+01, 1.4849439e+00])
+
+    result = test_system.compute_model(params)
+
+    print(result)
+
 if __name__ == '__main__':
-    test_add_and_clear_results()
+    #test_add_and_clear_results()
+    test_multi_planets()
