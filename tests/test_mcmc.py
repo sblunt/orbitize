f317dfd57303aef82a6226ce9db4e1ff71806664
Henry Ngo <hhkngo@gmail.com>
2019-07-16 12:27:31 -0700

    Merged PT and Ensemble MCMC tests into one (the only difference was the number of temps, which I made into an argument)

--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -3,35 +3,13 @@ import orbitize.sampler as sampler
 import orbitize.system as system
 import orbitize.read_input as read_input
 
-def test_pt_mcmc_runs(num_threads=1):
+def test_mcmc_runs(num_temps=0, num_threads=1):
     """
-    Tests the PTMCMC sampler by making sure it even runs
-    """
-    # use the test_csv dir
-    testdir = os.path.dirname(os.path.abspath(__file__))
-    input_file = os.path.join(testdir, 'test_val.csv')
-    data_table = read_input.read_file(input_file)
-    # Manually set 'object' column of data table
-    data_table['object'] = 1
-
-    # construct the system
-    orbit = system.System(1, data_table, 1, 0.01)
-
-    # construct sampler
-    n_temps=2
-    n_walkers=100
-    mcmc = sampler.MCMC(orbit, n_temps, n_walkers, num_threads=num_threads)
-
-    # run it a little (tests 0 burn-in steps)
-    mcmc.run_sampler(100)
-    # run it a little more
-    mcmc.run_sampler(1000, 1)
-    # run it a little more (tests adding to results object)
-    mcmc.run_sampler(1000, 1)
-
-def test_ensemble_mcmc_runs(num_threads=1):
-    """
-    Tests the EnsembleMCMC sampler by making sure it even runs
+    Tests the MCMC sampler by making sure it even runs
+    Args:
+        num_temps: Number of temperatures to use
+            Uses Parallel Tempering MCMC (ptemcee) if > 1, 
+            otherwises, uses Affine-Invariant Ensemble Sampler (emcee)
     """
     # use the test_csv dir
     testdir = os.path.dirname(os.path.abspath(__file__))
@@ -45,18 +23,19 @@ def test_ensemble_mcmc_runs(num_threads=1):
 
     # construct sampler
     n_walkers=100
-    mcmc = sampler.MCMC(orbit, 0, n_walkers, num_threads=num_threads)
-
+    mcmc = sampler.MCMC(orbit, num_temps, n_walkers, num_threads=num_threads)
 
     # run it a little (tests 0 burn-in steps)
     mcmc.run_sampler(100)
     # run it a little more
-    mcmc.run_sampler(1000, burn_steps=1)
+    mcmc.run_sampler(1000, 1)
     # run it a little more (tests adding to results object)
-    mcmc.run_sampler(1000, burn_steps=1)
+    mcmc.run_sampler(1000, 1)
 
 if __name__ == "__main__":
-    test_pt_mcmc_runs(num_threads=1)
-    test_pt_mcmc_runs(num_threads=4)
-    test_ensemble_mcmc_runs(num_threads=1)
-    test_ensemble_mcmc_runs(num_threads=8)
+    # Parallel Tempering tests
+    test_mcmc_runs(num_temps=10, num_threads=1)
+    test_mcmc_runs(num_temps=10, num_threads=4)
+    # Ensemble MCMC tests
+    test_mcmc_runs(num_temps=0, num_threads=1)
+    test_mcmc_runs(num_temps=0, num_threads=8)
