3e6c51bc978520692932b4e0f03b26c701b08643
Rob685 <rtejada@stanford.edu>
2019-07-17 13:48:40 -0700

    changes to kepler.py and system.py for rv predictions (tested)

--- orbitize/kepler.py
+++ orbitize/kepler.py
@@ -77,6 +77,7 @@ def calc_orbit(epochs, sma, ecc, inc, aop, pan, tau, plx, mtot, mass_for_Kamp=No
     # compute the true anomalies (size: n_orbs x n_dates)
     # Note: matrix multiplication makes the shapes work out here and below
     tanom = 2.*np.arctan(np.sqrt((1.0 + ecc)/(1.0 - ecc))*np.tan(0.5*eanom))
+    print(tanom)
     # compute 3-D orbital radius of second body (size: n_orbs x n_dates)
     radius = sma * (1.0 - ecc * np.cos(eanom))
 
@@ -103,13 +104,9 @@ def calc_orbit(epochs, sma, ecc, inc, aop, pan, tau, plx, mtot, mass_for_Kamp=No
     Kv = Kv.to(u.km/u.s)
 
     # compute the vz
-    # Rob: inserting negative value to obtain to account for the stellar argp
     vz = -Kv.value * (ecc*np.cos(aop) + np.cos(aop + tanom))
-    print(vz)
-
     # Squeeze out extra dimension (useful if n_orbs = 1, does nothing if n_orbs > 1)
     vz = np.squeeze(vz)[()]
-
     return raoff, deoff, vz
 
 
--- orbitize/test_space.py
+++ orbitize/test_space.py
@@ -3,13 +3,15 @@ import system as sys
 import read_input
 import pandas as pd
 
-data = pd.DataFrame({'epoch': np.arange(0, 200, 10), 'object': np.zeros(20), 'rv': np.ones(20), 'rv_err': np.ones(20)},
-                    columns=['epoch', 'object', 'raoff', 'raoff_err', 'decoff', 'decoff_err', 'rv', 'rv_err'])
-data['object'] = data['object'].astype(int)
-data.to_csv('/Users/Helios/orbitize/tests/mytestdata.csv', index=False)
+# data = pd.DataFrame({'epoch': np.arange(0, 200, 10), 'object': np.ones(20),
+# 'raoff': np.ones(20), 'raoff_err': np.ones(20),
+# 'decoff': np.ones(20), 'decoff_err': np.ones(20)},
+# columns=['epoch', 'object', 'raoff', 'raoff_err', 'decoff', 'decoff_err', 'rv', 'rv_err'])
+#data['object'] = data['object'].astype(int)
+#data.to_csv('/Users/Helios/orbitize/tests/mytestdata.csv', index=False)
 #data = read_input.read_file('/Users/Helios/orbitize/tests/test_val.csv')
 data = read_input.read_file('/Users/Helios/orbitize/tests/mytestdata.csv')
-print(data)  # read_input will only read 'rv','rv_err' named columns
+print(data)  # read_input will only read 'rv','rvß_err' named columns
 
 output_1 = sys.System(1, data, 1.0, 40, mass_err=0.01, fit_secondary_mass=True,
                       tau_ref_epoch=0, gamma_bounds=(-100, 100), jitter_bounds=(0, 20))
@@ -23,9 +25,9 @@ print("The separation indices are:", output_1.seppa)
 argument of periastron 1, position angle of nodes 1,
 epoch of periastron passage 1,"""
 
-params_arr = np.array([1.0, 0.2, np.pi/4, np.pi, np.pi/4, 0, 40, 0.0, 2, 0.1, 1.0])
+params_arr = np.array([1.0, 0.2, np.pi/4, np.pi/4, np.pi, 0.0, 40, 0.0, 2,
+                       0.1, 1.0])
 # print(params_arr.shape)
 
 model = output_1.compute_model(params_arr)
-
 print(model)
