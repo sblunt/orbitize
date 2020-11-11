59c7f591f6d6a2b30fb4ff7a88095ae392cdc732
sblunt <sarah.blunt.3@gmail.com>
2020-11-11 10:53:06 -0800

    compute_sep now takes in m0 instead of mtot

--- orbitize/radvel_utils/compute_sep.py
+++ orbitize/radvel_utils/compute_sep.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
+from astropy import units as u
 
 from radvel.basis import Basis
+from radvel.utils import Msini
 from orbitize.basis import t0_to_tau
 from orbitize.kepler import calc_orbit
 
 def compute_sep(
-    df, epochs, basis, mtot, mtot_err, plx, plx_err, n_planets=1, pl_num=1
+    df, epochs, basis, m0, m0_err, plx, plx_err, n_planets=1, pl_num=1
 ):
     """
     Computes a sky-projected angular separation posterior given a 
@@ -18,8 +20,8 @@ def compute_sep(
             separations
         basis (str): basis string of input posterior (see 
             radvel.basis.BASIS_NAMES` for the full list of possibilities). 
-        mtot (float): median of total mass distribution (assumed Gaussian).
-        mtot_err (float): 1sigma error of total mass distribution 
+        m0 (float): median of primary mass distribution (assumed Gaussian).
+        m0_err (float): 1sigma error of primary mass distribution 
             (assumed Gaussian).
         plx (float): median of parallax distribution (assumed Gaussian).
         plx_err: 1sigma error of parallax distribution (assumed Gaussian).
@@ -29,8 +31,8 @@ def compute_sep(
 
     Example:
 
-        >> df = pd.read_csv('sample_radvel_chains.csv.bz2', index_col=0)
-        >> epochs = Time([2022, 2024], format='decimalyear')
+        >> df = pandas.read_csv('sample_radvel_chains.csv.bz2', index_col=0)
+        >> epochs = astropy.time.Time([2022, 2024], format='decimalyear')
         >> seps, df_orb = compute_sep(
                df, epochs, 'per tc secosw sesinw k', 0.82, 0.02, 312.22, 0.47
            )
@@ -47,24 +49,29 @@ def compute_sep(
     chain_len = len(df)
     tau_ref_epoch = 58849
 
-    # convert RadVel parameters
+    # convert RadVel posteriors -> orbitize posteriors
+    m_st = np.random.normal(m0, m0_err, size = chain_len)
+    semiamp = df['k{}'.format(pl_num)].values
     per_day = df['per{}'.format(pl_num)].values
     period_yr = per_day / 365.25
     ecc = df['e{}'.format(pl_num)].values
+    msini = (
+        Msini(semiamp, per_day, m_st, ecc, Msini_units='Earth') * 
+        (u.M_earth / u.M_sun).to('')
+    )
+    cosi = (2. * np.random.random(size = chain_len)) - 1.
+    inc = np.arccos(cosi)
+    m_pl = msini / np.sin(inc)
+    mtot = m0 + m_pl
+    sma = (period_yr**2 * mtot)**(1/3)
     omega_st_rad = df['w{}'.format(pl_num)].values
+    omega_pl_rad = omega_st_rad + np.pi
+    parallax = np.random.normal(plx, plx_err, size = chain_len)
+    lan = np.random.random_sample(size = chain_len) * 2. * np.pi
     tp_mjd = df['tp{}'.format(pl_num)].values - 2400000.5
     tau = t0_to_tau(tp_mjd, tau_ref_epoch, period_yr)
 
-    # generate 
-    mtot = np.random.normal(mtot, mtot_err, size = chain_len)
-    sma = (period_yr**2 * mtot)**(1/3)
-    omega_pl_rad = omega_st_rad + np.pi
-    parallax = np.random.normal(plx, plx_err, size = chain_len)
-    cosi = (2. * np.random.random(size = chain_len)) - 1.
-    inc = np.arccos(cosi)
-    lan = np.random.random(size = chain_len) * 2. * np.pi
-
-    # returns raoff, deoff in mas
+    # compute projected separation in mas
     raoff, deoff, _ = calc_orbit(
         epochs.mjd, sma, ecc, inc, 
         omega_pl_rad, lan, tau, 
