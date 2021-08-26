659e5aeaca59e7116fe7ff37e9985bd34d26abb3
Sarah Blunt <sblunt@caltech.edu>
2021-08-25 17:17:45 -0700

    add preliminary nielsen iad test

--- tests/test_hipparcos.py
+++ tests/test_hipparcos.py
@@ -1,3 +1,7 @@
+import numpy as np
+import emcee
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 from orbitize.hipparcos import HipparcosLogProb
 
 def test_hipparcos_api():
@@ -6,13 +10,99 @@ def test_hipparcos_api():
     hip_num = '25'
     num_secondary_bodies = 1
     iad_file = '/data/user/sblunt/HipIAD/H{}/HIP{}.d'.format(hip_num[0:3], hip_num)
-    iad_file = '/data/user/sblunt/HipIAD/H{}/HIP{}.d'.format(hip_num[0:3], hip_num)
 
     try:
         myHip = HipparcosLogProb(iad_file, hip_num, num_secondary_bodies)
     except Exception: 
         pass
 
+def nielsen_iad_test(hip_num='027321', saveplot='foo.png'):
+
+    # reproduce the test from Nielsen+ 202 (end of Section 3.1)
+    
+    num_secondary_bodies = 0
+    iad_file = '/data/user/sblunt/HipIAD/H{}/HIP{}.d'.format(hip_num[0:3], hip_num)
+    myHipLogProb = HipparcosLogProb(iad_file, hip_num, num_secondary_bodies)
+    n_epochs = len(myHipLogProb.epochs)
+
+    def log_prob(model_pars):
+        ra_model = np.zeros(n_epochs)
+        dec_model = np.zeros(n_epochs)
+        lnlike = myHipLogProb.compute_lnlike(ra_model, dec_model, model_pars)
+        return lnlike
+    
+    ndim, nwalkers = 5, 100
+
+    # initialize walkers
+    # fitting alpha_H0, delta_H0, plx, mu_a, mu_d
+    p0 = np.random.randn(nwalkers, ndim)
+
+    # plx
+    p0[:,2] *= myHipLogProb.plx0_err
+    p0[:,2] += myHipLogProb.plx0
+
+    # PM
+    p0[:,3] *= myHipLogProb.pm_ra0
+    p0[:,3] += myHipLogProb.pm_ra0_err
+    p0[:,4] *= myHipLogProb.pm_dec0
+    p0[:,4] += myHipLogProb.pm_dec0_err
+
+    # set up an MCMC
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob)
+    print('Starting burn-in!')
+    state = sampler.run_mcmc(p0, 100)
+    sampler.reset()
+    print('Starting production chain!')
+    sampler.run_mcmc(state, 1000)
+
+
+    if saveplot is not None:
+        _, axes = plt.subplots(5, figsize=(5,12))
+
+        # RA offset
+        axes[0].hist(sampler.flatchain[:,0], bins=50, normed=True)
+        xs = np.linspace(-1, 1, 1000)
+        axes[0].plot(xs, norm(0, myHipLogProb.alpha0_err).pdf(xs))
+        axes[0].set_xlabel('RA Offset [mas]')
+
+        # Dec offset
+        axes[1].hist(sampler.flatchain[:,1], bins=50, normed=True)
+        axes[1].plot(xs, norm(0, myHipLogProb.delta0_err).pdf(xs))
+        axes[1].set_xlabel('Dec Offset [mas]')
+
+        # plx
+        xs = np.linspace(
+            myHipLogProb.plx0 - 3 * myHipLogProb.plx0_err, 
+            myHipLogProb.plx0 + 3 * myHipLogProb.plx0_err,
+            1000
+        )
+        axes[2].hist(sampler.flatchain[:,2], bins=50, normed=True)
+        axes[2].plot(xs, norm(myHipLogProb.plx0, myHipLogProb.plx0_err).pdf(xs))
+        axes[2].set_xlabel('plx [mas]')
+
+        # PM RA
+        xs = np.linspace(
+            myHipLogProb.pm_ra0 - 3 * myHipLogProb.pm_ra0_err, 
+            myHipLogProb.pm_ra0 + 3 * myHipLogProb.pm_ra0_err,
+            1000
+        )
+        axes[3].hist(sampler.flatchain[:,3], bins=50, normed=True)
+        axes[3].plot(xs, norm(myHipLogProb.pm_ra0, myHipLogProb.pm_ra0_err).pdf(xs))
+        axes[3].set_xlabel('PM RA [mas/yr]')
+
+        # PM Dec
+        xs = np.linspace(
+            myHipLogProb.pm_dec0 - 3 * myHipLogProb.pm_dec0_err, 
+            myHipLogProb.pm_dec0 + 3 * myHipLogProb.pm_dec0_err,
+            1000
+        )
+        axes[4].hist(sampler.flatchain[:,4], bins=50, normed=True)
+        axes[4].plot(xs, norm(myHipLogProb.pm_dec0, myHipLogProb.pm_dec0_err).pdf(xs))
+        axes[4].set_xlabel('PM Dec [mas/yr]')
+
+        plt.tight_layout()
+        plt.savefig(saveplot, dpi=250)
+
 
 if __name__ == '__main__':
-    test_hipparcos_api()
+    nielsen_iad_test()
