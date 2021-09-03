79030fa394d75ae0e1eff296bd3f2fd810fb4f53
Sarah Blunt <sblunt@caltech.edu>
2021-09-03 16:59:02 -0700

    allow errors in nbs when building docs

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,9 @@ extensions = [
 # Disable notebook timeout
 nbsphinx_timeout = -1
 
+# Allow notebook errors
+nbsphinx_allow_errors = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
