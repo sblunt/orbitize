4f3330300b7f0cf1e6ddd4a3b128bdd66eab6118
devincody <devincody@gmail.com>
2021-10-02 23:21:22 -0700

    removing python2.7, pedagogical fluff

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ else:
         print("Error: Importing cython build environment failed")
         USE_C_KEPLER_MODULE = 0
 
-
 # auto-updating version code stolen from RadVel
 def get_property(prop, project):
     result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
@@ -47,16 +46,9 @@ setup(
     include_package_data = True,
     zip_safe=False,
     classifiers=[
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Astronomy',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: BSD License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         ],
     keywords='Orbits Astronomy Astrometry',
