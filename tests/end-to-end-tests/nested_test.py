c0cc7b82ad7fcbab987f58188947ff721127a9d0
Sarah Blunt <sarah.blunt.3@gmail.com>
2023-07-05 13:49:52 -0700

    change savdir of nested_test

--- tests/end-to-end-tests/nested_test.py
+++ tests/end-to-end-tests/nested_test.py
@@ -3,8 +3,7 @@ from orbitize import read_input, system, priors, sampler
 import matplotlib.pyplot as plt
 
 
-data_table = read_input.read_file('{}/GJ504.csv'.format(orbitize.DATADIR))
-print(data_table)
+data_table = read_input.read_file("{}/GJ504.csv".format(orbitize.DATADIR))
 
 # number of secondary bodies in system
 num_planets = 1
@@ -15,8 +14,7 @@ mass_err = 0.08
 plx = 56.95
 plx_err = 0
 sys = system.System(
-    num_planets, data_table, total_mass,
-    plx, mass_err=mass_err, plx_err=plx_err
+    num_planets, data_table, total_mass, plx, mass_err=mass_err, plx_err=plx_err
 )
 # alias for convenience
 lab = sys.param_idx
@@ -24,23 +22,25 @@ lab = sys.param_idx
 mu = 0.2
 sigma = 0.05
 
-sys.sys_priors[lab['ecc1']] = priors.GaussianPrior(mu, sigma)
-sys.sys_priors[lab['inc1']] = 2.5
+sys.sys_priors[lab["ecc1"]] = priors.GaussianPrior(mu, sigma)
+sys.sys_priors[lab["inc1"]] = 2.5
 nested_sampler = sampler.NestedSampler(sys)
 
-samples, exec_time, num_iter = nested_sampler.run_sampler(static = True, bound = 'multi')
-nested_sampler.results.save_results('test34.hdf5')
+samples, exec_time, num_iter = nested_sampler.run_sampler(static=True, bound="multi")
+nested_sampler.results.save_results("test34.hdf5")
 print("execution time (min) is: " + str(exec_time))
 print("iteration number is: " + str(num_iter))
 
 plt.figure()
-accepted_eccentricities = nested_sampler.results.post[:, lab['ecc1']]
+accepted_eccentricities = nested_sampler.results.post[:, lab["ecc1"]]
 plt.hist(accepted_eccentricities)
-plt.xlabel('ecc'); plt.ylabel('number of orbits')
-plt.savefig('../../results/ecc_test34.png')
+plt.xlabel("ecc")
+plt.ylabel("number of orbits")
+plt.savefig("ecc_test34.png")
 
 plt.figure()
-accepted_inclinations = nested_sampler.results.post[:, lab['inc1']]
+accepted_inclinations = nested_sampler.results.post[:, lab["inc1"]]
 plt.hist(accepted_inclinations)
-plt.xlabel('inc'); plt.ylabel('number of orbits')
-plt.savefig('../../results/inc_test34.png')
+plt.xlabel("inc")
+plt.ylabel("number of orbits")
+plt.savefig("inc_test34.png")
