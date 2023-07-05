eccdf48b6521c3741fbaa112d97b4c5260cc498a
Sarah Blunt <sblunt@caltech.edu>
2023-07-05 15:29:56 -0700

    clean up unit test a bit; test both dynamic & static samplers

--- tests/test_nested_sampler.py
+++ tests/test_nested_sampler.py
@@ -15,49 +15,46 @@ from orbitize.system import generate_synthetic_data
 
 def test_nested_sampler():
     # generate data
-    mtot = 1.2 # total system mass [M_sol]
-    plx = 60.0 # parallax [mas]
+    mtot = 1.2  # total system mass [M_sol]
+    plx = 60.0  # parallax [mas]
     n_orbs = 500
-    sma = 2.3
-    data_table, orbit_fraction = generate_synthetic_data(mtot, plx, sma=sma, 
-    num_obs=30)
-    print('The orbit fraction is {}%'.format(np.round(orbit_fraction),1))
+    orbit_frac = 95
+    data_table, sma = generate_synthetic_data(
+        orbit_frac,
+        mtot,
+        plx,
+        num_obs=30,
+    )
 
-    # assumed ecc and uncertainty values
+    # assumed ecc value
     ecc = 0.5
-    unc = 0.1
 
     # initialize orbitize `System` object
     sys = system.System(1, data_table, mtot, plx)
-    print(data_table)
     lab = sys.param_idx
 
-    #set all parameters except eccentricity to fixed values for testing
-    sys.sys_priors[lab['inc1']] = np.pi/4
-    sys.sys_priors[lab['sma1']] = sma
-    sys.sys_priors[lab['aop1']] = 0.
-    sys.sys_priors[lab['pan1']] = 0.
-    sys.sys_priors[lab['tau1']] = 0.8
-    sys.sys_priors[lab['plx']] = plx
-    sys.sys_priors[lab['mtot']] = mtot
+    # set all parameters except eccentricity to fixed values for testing
+    sys.sys_priors[lab["inc1"]] = np.pi / 4
+    sys.sys_priors[lab["sma1"]] = sma
+    sys.sys_priors[lab["aop1"]] = 0.0
+    sys.sys_priors[lab["pan1"]] = 0.0
+    sys.sys_priors[lab["tau1"]] = 0.8
+    sys.sys_priors[lab["plx"]] = plx
+    sys.sys_priors[lab["mtot"]] = mtot
 
+    # run both static & dynamic nested samplers
+    static_sampler = sampler.NestedSampler(sys)
+    _ = static_sampler.run_sampler(n_orbs, bound="multi")
 
-    # run nested sampler
-    nested_sampler = sampler.NestedSampler(sys)
-    _ = nested_sampler.run_sampler(n_orbs, bound = 'multi')
+    dynamic_sampler = sampler.NestedSampler(sys)
+    _ = static_sampler.run_sampler(n_orbs, bound="multi", pfrac=0.5, static=False)
 
-    # save results
-    nested_sampler.results.save_results('test1.hdf5')
-    accepted_eccentricities = nested_sampler.results.post[:, lab['ecc1']]
+    static_eccentricities = static_sampler.results.post[:, lab["ecc1"]]
+    dynamic_eccentricities = dynamic_sampler.results.post[:, lab["ecc1"]]
 
-    assert accepted_eccentricities == pytest.approx(ecc, abs=unc)
+    assert static_eccentricities == pytest.approx(ecc, abs=0.1)
+    assert dynamic_eccentricities == pytest.approx(ecc, abs=0.1)
 
-
-
-
-
-#TO DO: add in a test that asserts that the recovered posterior is reasonable
 
 if __name__ == "__main__":
     test_nested_sampler()
-    print('Finished!')
