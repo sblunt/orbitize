76a7e0d6fe58d68f460454a69ca6e5a5f51a6bbf
Sarah Blunt <sblunt@caltech.edu>
2023-07-05 16:00:45 -0700

    getting unit tests to pass

--- tests/test_nested_sampler.py
+++ tests/test_nested_sampler.py
@@ -27,11 +27,13 @@ def test_nested_sampler():
     sys = system.System(1, data_table, mtot, plx)
     lab = sys.param_idx
 
-    # set all parameters except eccentricity to fixed values for testing
+    ecc = 0.5  # eccentricity
+
+    # set all parameters except eccentricity to fixed values (same as used to generate data)
     sys.sys_priors[lab["inc1"]] = np.pi / 4
     sys.sys_priors[lab["sma1"]] = sma
-    sys.sys_priors[lab["aop1"]] = 0.0
-    sys.sys_priors[lab["pan1"]] = 0.0
+    sys.sys_priors[lab["aop1"]] = np.pi / 4
+    sys.sys_priors[lab["pan1"]] = np.pi / 4
     sys.sys_priors[lab["tau1"]] = 0.8
     sys.sys_priors[lab["plx"]] = plx
     sys.sys_priors[lab["mtot"]] = mtot
@@ -41,13 +43,13 @@ def test_nested_sampler():
     _ = dynamic_sampler.run_sampler(bound="multi", pfrac=0.95, static=False)
 
     dynamic_eccentricities = dynamic_sampler.results.post[:, lab["ecc1"]]
-    assert dynamic_eccentricities == pytest.approx(ecc, abs=0.1)
+    assert np.median(dynamic_eccentricities) == pytest.approx(ecc, abs=0.1)
 
     static_sampler = sampler.NestedSampler(sys)
     _ = static_sampler.run_sampler(bound="multi")
 
     static_eccentricities = static_sampler.results.post[:, lab["ecc1"]]
-    assert static_eccentricities == pytest.approx(ecc, abs=0.1)
+    assert np.median(static_eccentricities) == pytest.approx(ecc, abs=0.1)
 
     # check that the static sampler raises an error when user tries to set pfrac
     try:
