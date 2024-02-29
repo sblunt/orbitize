0eac480f86eece5ba9db842b9226a7c40b2e90b5
Sarah Blunt <sarah.blunt.3@gmail.com>
2024-02-29 10:30:06 -0600

    lint

--- orbitize/priors.py
+++ orbitize/priors.py
@@ -20,7 +20,7 @@ class Prior(abc.ABC):
     """
 
     is_correlated = False
-    
+
     @abc.abstractmethod
     def draw_samples(self, num_samples):
         pass
@@ -29,25 +29,27 @@ class Prior(abc.ABC):
     def compute_lnprob(self, element_array):
         pass
 
+
 class NearestNDInterpPrior(Prior):
     """
     Nearest Neighbor interp. This class is
     a wrapper for scipy.interpolate.NearestNDInterpolator.
 
     Args:
-        interp_fct (scipy.interpolate.NearestNDInterpolator): scipy Interpolator 
+        interp_fct (scipy.interpolate.NearestNDInterpolator): scipy Interpolator
             object containing the NDInterpolator defined by the user
-        total_params (float): number of parameters 
+        total_params (float): number of parameters
 
     Written: Jorge LLop-Sayson (2021)
     """
+
     is_correlated = True
 
     def __init__(self, interp_fct, total_params):
         self.interp_fct = interp_fct
         self.total_params = total_params
         self.param_num = 0
-        self.correlated_drawn_samples = None 
+        self.correlated_drawn_samples = None
         self.correlated_input_samples = None
         self.num_priorsFromArr = interp_fct.values.size
         self.ind_draw = None
@@ -69,27 +71,27 @@ class NearestNDInterpPrior(Prior):
             num_samples (float): the number of samples to generate.
 
         Returns:
-            numpy array of float: samples drawn from the ND interpolator 
+            numpy array of float: samples drawn from the ND interpolator
             distribution. Array has length `num_samples`.
         """
         if self.param_num == 0:
-            ind_draw = np.random.randint(self.num_priorsFromArr,size=num_samples)
+            ind_draw = np.random.randint(self.num_priorsFromArr, size=num_samples)
             self.ind_draw = ind_draw
-            return_me = self.interp_fct.points[self.ind_draw,self.param_num]
+            return_me = self.interp_fct.points[self.ind_draw, self.param_num]
             self.increment_param_num()
             return return_me
         else:
-            return_me = self.interp_fct.points[self.ind_draw,self.param_num]
+            return_me = self.interp_fct.points[self.ind_draw, self.param_num]
             self.increment_param_num()
             return return_me
 
     def compute_lnprob(self, element_array):
         """
-        Compute log(probability) of an array of numbers wrt a the defined ND 
+        Compute log(probability) of an array of numbers wrt a the defined ND
         interpolator. Negative numbers return a probability of -inf.
 
         Args:
-            element_array (float or np.array of float): array of numbers. We want 
+            element_array (float or np.array of float): array of numbers. We want
                 the probability of drawing each of these from the ND interpolator.
 
         Returns:
@@ -100,8 +102,10 @@ class NearestNDInterpPrior(Prior):
         if self.param_num == 0:
             self.correlated_input_samples = element_array
         else:
-            self.correlated_input_samples = np.append(self.correlated_input_samples, element_array)
-        if self.param_num == self.total_params-1:
+            self.correlated_input_samples = np.append(
+                self.correlated_input_samples, element_array
+            )
+        if self.param_num == self.total_params - 1:
             lnlike = self.interp_fct(self.correlated_input_samples)
             self.increment_param_num()
             self.logparam_corr = 1
@@ -110,38 +114,40 @@ class NearestNDInterpPrior(Prior):
             self.increment_param_num()
             return 0
 
+
 class KDEPrior(Prior):
     """
     Gaussian kernel density estimation (KDE) prior. This class is
     a wrapper for scipy.stats.gaussian_kde.
 
     Args:
-        gaussian_kde (scipy.stats.gaussian_kde): scipy KDE object containing the 
+        gaussian_kde (scipy.stats.gaussian_kde): scipy KDE object containing the
             KDE defined by the user
         total_params (float): number of parameters in the KDE
-        bounds (array_like, optional): bounds for the KDE out of which the prob 
+        bounds (array_like, optional): bounds for the KDE out of which the prob
             returned is -Inf
-        bounds (array_like of bool, optional): if True for a parameter the 
+        bounds (array_like of bool, optional): if True for a parameter the
             parameter is fit to the KDE in log-scale
 
     Written: Jorge LLop-Sayson, Sarah Blunt (2021)
     """
+
     is_correlated = True
-    
+
     def __init__(self, gaussian_kde, total_params, bounds=[], log_scale_arr=[]):
         self.gaussian_kde = gaussian_kde
         self.total_params = total_params
         self.param_num = 0
         self.logparam_corr = 1
         if not bounds:
-            self.bounds = [[-np.inf,np.inf] for i in range(total_params)]
+            self.bounds = [[-np.inf, np.inf] for i in range(total_params)]
         else:
             self.bounds = bounds
         if not log_scale_arr:
-            self.log_scale_arr = [False for i in range(total_params)] 
+            self.log_scale_arr = [False for i in range(total_params)]
         else:
             self.log_scale_arr = log_scale_arr
-        self.correlated_drawn_samples = None 
+        self.correlated_drawn_samples = None
         self.correlated_input_samples = None
 
     def __repr__(self):
@@ -164,7 +170,7 @@ class KDEPrior(Prior):
             num_samples (float): the number of samples to generate.
 
         Returns:
-            numpy array of float: samples drawn from the KDE 
+            numpy array of float: samples drawn from the KDE
             distribution. Array has length `num_samples`.
         """
         if self.param_num == 0:
@@ -175,7 +181,7 @@ class KDEPrior(Prior):
             return_me = self.correlated_drawn_samples[self.param_num]
             self.increment_param_num()
             return return_me
-            
+
     def compute_lnprob(self, element_array):
         """
         Compute log(probability) of an array of numbers wrt a the defined KDE.
@@ -190,28 +196,35 @@ class KDEPrior(Prior):
             corresponding to the probability of drawing each of the numbers
             in the input `element_array`.
         """
-        if element_array<self.bounds[self.param_num][0] or element_array>self.bounds[self.param_num][1]:
+        if (
+            element_array < self.bounds[self.param_num][0]
+            or element_array > self.bounds[self.param_num][1]
+        ):
             if self.log_scale_arr[self.param_num]:
                 element_array_lin = element_array
                 element_array = np.log10(element_array)
                 if np.isnan(element_array):
-                    element_array = 0 #set to zero bc doesn't matter what it is since we're already returning a small prob
+                    element_array = 0  # set to zero bc doesn't matter what it is since we're already returning a small prob
             if self.param_num == 0:
                 self.correlated_input_samples = element_array
             else:
-                self.correlated_input_samples = np.append(self.correlated_input_samples, element_array)
+                self.correlated_input_samples = np.append(
+                    self.correlated_input_samples, element_array
+                )
             self.increment_param_num()
             self.logparam_corr = 1
             return -1e10
         if self.log_scale_arr[self.param_num]:
             element_array_lin = element_array
             element_array = np.log10(element_array)
-            self.logparam_corr = self.logparam_corr*(element_array_lin)
+            self.logparam_corr = self.logparam_corr * (element_array_lin)
         if self.param_num == 0:
             self.correlated_input_samples = element_array
         else:
-            self.correlated_input_samples = np.append(self.correlated_input_samples, element_array)
-        if self.param_num == self.total_params-1:
+            self.correlated_input_samples = np.append(
+                self.correlated_input_samples, element_array
+            )
+        if self.param_num == self.total_params - 1:
             lnlike = self.gaussian_kde.logpdf(self.correlated_input_samples)
             self.increment_param_num()
             self.logparam_corr = 1
@@ -219,6 +232,7 @@ class KDEPrior(Prior):
         else:
             self.increment_param_num()
             return 0
+
 
 class GaussianPrior(Prior):
     """Gaussian prior.
@@ -257,9 +271,7 @@ class GaussianPrior(Prior):
             Gaussian distribution. Array has length `num_samples`.
         """
 
-        samples = np.random.normal(
-            loc=self.mu, scale=self.sigma, size=num_samples
-        )
+        samples = np.random.normal(loc=self.mu, scale=self.sigma, size=num_samples)
         bad = np.inf
 
         if self.no_negatives:
@@ -290,7 +302,7 @@ class GaussianPrior(Prior):
             corresponding to the probability of drawing each of the numbers
             in the input `element_array`.
         """
-        lnprob = - 0.5*((element_array - self.mu) / self.sigma)**2
+        lnprob = -0.5 * ((element_array - self.mu) / self.sigma) ** 2
 
         if self.no_negatives:
 
@@ -355,14 +367,16 @@ class LogUniformPrior(Prior):
         """
         normalizer = self.logmax - self.logmin
 
-        lnprob = -np.log((element_array*normalizer))
+        lnprob = -np.log((element_array * normalizer))
 
         # account for scalar inputs
         if np.shape(lnprob) == ():
             if (element_array > self.maxval) or (element_array < self.minval):
                 lnprob = -np.inf
         else:
-            lnprob[(element_array > self.maxval) | (element_array < self.minval)] = -np.inf
+            lnprob[(element_array > self.maxval) | (element_array < self.minval)] = (
+                -np.inf
+            )
 
         return lnprob
 
@@ -409,14 +423,16 @@ class UniformPrior(Prior):
         Returns:
             np.array: array of prior probabilities
         """
-        lnprob = np.log(np.ones(np.size(element_array))/(self.maxval - self.minval))
+        lnprob = np.log(np.ones(np.size(element_array)) / (self.maxval - self.minval))
 
         # account for scalar inputs
         if np.shape(lnprob) == ():
             if (element_array > self.maxval) or (element_array < self.minval):
                 lnprob = -np.inf
         else:
-            lnprob[(element_array > self.maxval) | (element_array < self.minval)] = -np.inf
+            lnprob[(element_array > self.maxval) | (element_array < self.minval)] = (
+                -np.inf
+            )
 
         return lnprob
 
@@ -462,9 +478,9 @@ class SinPrior(Prior):
         Returns:
             np.array: array of prior probabilities
         """
-        normalization = 2.
+        normalization = 2.0
 
-        lnprob = np.log(np.sin(element_array)/normalization)
+        lnprob = np.log(np.sin(element_array) / normalization)
 
         # account for scalar inputs
         if np.shape(lnprob) == ():
@@ -510,22 +526,24 @@ class LinearPrior(Prior):
         Returns:
             np.array:  samples ranging from [0, -b/m) as floats.
         """
-        norm = -0.5*self.b**2/self.m
+        norm = -0.5 * self.b**2 / self.m
 
         # draw uniform from 0 to 1
         samples = np.random.uniform(0, 1, num_samples)
 
         # generate samples following a linear distribution
-        linear_samples = -np.sqrt(2.*norm*samples/self.m + (self.b/self.m)**2) - (self.b/self.m)
+        linear_samples = -np.sqrt(
+            2.0 * norm * samples / self.m + (self.b / self.m) ** 2
+        ) - (self.b / self.m)
 
         return linear_samples
 
     def compute_lnprob(self, element_array):
 
-        x_intercept = -self.b/self.m
-        normalizer = -0.5*self.b**2/self.m
+        x_intercept = -self.b / self.m
+        normalizer = -0.5 * self.b**2 / self.m
 
-        lnprob = np.log((self.m*element_array + self.b)/normalizer)
+        lnprob = np.log((self.m * element_array + self.b) / normalizer)
 
         # account for scalar inputs
         if np.shape(lnprob) == ():
@@ -536,19 +554,21 @@ class LinearPrior(Prior):
 
         return lnprob
 
+
 class ObsPrior(Prior):
     """
 
     TODO: document better, create notebook tutorial where I plot these priors
         vs standard priors, finish tests
 
-    Limitations: 
+    Limitations:
     - in current form, only works with MCMC
     - in current form, only works with planetary astrometry only (no RVs or other data types)
     - in current form, only works when input astrometry is Ra/Dec (ie need to convert ahead of time)
     - must let ecc, sma, and tau float, but must fix plx and mtot
     - only works with one secondary object
     """
+
     def __init__(self, epochs, ra_err, dec_err, mtot, plx, tau_ref_epoch=58849):
         self.epochs = epochs
         self.tau_ref_epoch = tau_ref_epoch
@@ -586,7 +606,7 @@ class ObsPrior(Prior):
 
     def draw_samples(self, num_samples):
 
-        # for now, draw samples from a distribution uniform in log(a), ecc, and tau 
+        # for now, draw samples from a distribution uniform in log(a), ecc, and tau
         # this is needed for initializing the MCMC walkers
         samples = self.draw_uniform_samples(num_samples)
         self.increment_param_num()
@@ -608,19 +628,17 @@ class ObsPrior(Prior):
             ecc = self.correlated_input_samples[1]
             tau = self.correlated_input_samples[2]
 
-            if ((sma < 0) or (ecc < 0) or (ecc > 1) or (tau < 0) or (tau > 1)):
+            if (sma < 0) or (ecc < 0) or (ecc > 1) or (tau < 0) or (tau > 1):
                 self.increment_param_num()
                 return -np.inf
-            
+
             period = np.sqrt(
-                4 * np.pi**2 * (sma * u.au)**3 / 
-                (cst.G * (self.mtot * u.Msun))
+                4 * np.pi**2 * (sma * u.au) ** 3 / (cst.G * (self.mtot * u.Msun))
             )
 
-            jac_prefactor = -((
-                (cst.G * self.mtot * u.Msun)**2 * period / 
-                (2 * np.pi**4)
-            )**(1 / 3)).value
+            jac_prefactor = -(
+                ((cst.G * self.mtot * u.Msun) ** 2 * period / (2 * np.pi**4)) ** (1 / 3)
+            ).value
 
             meananom = basis.tau_to_manom(
                 self.epochs, sma, self.mtot, tau, self.tau_ref_epoch
@@ -629,12 +647,13 @@ class ObsPrior(Prior):
 
             # sum Jacobian over all epochs (O'Neil 2019 eq 33)
             jacobian = np.sum(
-                (1 / (self.ra_err * self.dec_err)) *
-                np.abs(
-                    2 * (ecc**2 - 2) * np.sin(eccanom) +
-                    ecc * (3 * meananom + np.sin(2 * eccanom)) +
-                    3 * meananom * np.cos(eccanom)
-                ) / (6 * np.sqrt(1 - ecc**2))
+                (1 / (self.ra_err * self.dec_err))
+                * np.abs(
+                    2 * (ecc**2 - 2) * np.sin(eccanom)
+                    + ecc * (3 * meananom + np.sin(2 * eccanom))
+                    + 3 * meananom * np.cos(eccanom)
+                )
+                / (6 * np.sqrt(1 - ecc**2))
             )
 
             jacobian *= np.abs(jac_prefactor)
@@ -660,19 +679,19 @@ def all_lnpriors(params, priors):
     Returns:
         float: prior probability of this set of parameters
     """
-    logp = 0.
+    logp = 0.0
 
     for param, prior in zip(params, priors):
         param = np.array([param])
-    
+
         logp += prior.compute_lnprob(param)  # retrun a float
 
     return logp
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    myPrior = LinearPrior(-1., 1.)
+    myPrior = LinearPrior(-1.0, 1.0)
     mySamples = myPrior.draw_samples(1000)
     print(mySamples)
     myProbs = myPrior.compute_lnprob(mySamples)
