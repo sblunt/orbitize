4d14d011905a6f4c5db756b6bc1a89f94bd210ce
Farrah Molina <farrahashmolina1055@gmail.com>
2024-10-18 13:46:48 -0500

    mcmc test

---
+++ mcmc_test.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import orbitize
+from orbitize import driver
+import multiprocessing as mp
+
+filename = "{}/GJ504.csv".format(orbitize.DATADIR)
+
+# system parameters
+num_secondary_bodies = 1
+total_mass = 1.75  # [Msol]
+plx = 51.44  # [mas]
+mass_err = 0.05  # [Msol]
+plx_err = 0.12  # [mas]
+
+# MCMC parameters
+num_temps = 5
+num_walkers = 20
+num_threads = 2  # or a different number if you prefer, mp.cpu_count() for example
+
+
+my_driver = driver.Driver(
+    filename,
+    "MCMC",
+    num_secondary_bodies,
+    total_mass,
+    plx,
+    mass_err=mass_err,
+    plx_err=plx_err,
+    mcmc_kwargs={
+        "num_temps": num_temps,
+        "num_walkers": num_walkers,
+        "num_threads": num_threads,
+    },
+)
+total_orbits = 6000  # number of steps x number of walkers (at lowest temperature)
+burn_steps = 10  # steps to burn in per walker
+thin = 2  # only save every 2nd step
+
+my_driver.sampler.run_sampler(total_orbits, burn_steps=burn_steps, thin=thin)
