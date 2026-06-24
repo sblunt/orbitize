ec46224245f375a3346865ef729102d073850828
Farrah Molina <farrahashmolina1055@gmail.com>
2026-06-24 00:10:23 -0500

    Moved to CIERA-Research

--- tests/save_chain_corner_plot.py
+++
@@ -1,9 +0,0 @@
-# Save Chains Corner Plot
-import orbitize.results
-
-import matplotlib.pyplot as plt
-
-myResults = orbitize.results.Results()  # create empty Results object
-myResults.load_results("my_posterior.hdf5")  # load from file
-
-corner_figure = myResults.plot_corner()
