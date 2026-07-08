cee01e477832470a5b83132bdcb098c8840d19c3
Sarah Blunt <sarah.blunt.3@gmail.com>
2026-07-07 17:23:56 -0700

    Revert "in-progress unit debugging script"
    This reverts commit d8ce782c2f0c880971ab189a09d7a7df00dac806.

--- tests/test_rv_plotting.py
+++ tests/test_rv_plotting.py
@@ -1,10 +1,8 @@
-from orbitize import driver, DATADIR, plot, system
+from orbitize import driver, DATADIR, plot
 import multiprocessing as mp
-import numpy as np
-import astropy.units as u, astropy.constants as cst
 
 
-def test_rv_default_inst(save_plot=False):
+def test_rv_default_inst():
     # Initialize Driver to Run MCMC
     filename = "{}/HD4747.csv".format(DATADIR)
 
@@ -15,7 +13,7 @@ def test_rv_default_inst(save_plot=False):
     plx_err = 0.12  # [mas]
 
     num_temps = 5
-    num_walkers = 50
+    num_walkers = 30
     num_threads = mp.cpu_count()  # or a different number if you prefer
 
     my_driver = driver.Driver(
@@ -34,7 +32,7 @@ def test_rv_default_inst(save_plot=False):
         },
     )
 
-    total_orbits = 100
+    total_orbits = 1000
     burn_steps = 10
     thin = 2
 
@@ -42,17 +40,6 @@ def test_rv_default_inst(save_plot=False):
     m = my_driver.sampler
     m.run_sampler(total_orbits, burn_steps=burn_steps, thin=thin)
     epochs = my_driver.system.data_table["epoch"]
-
-    # Set all posterior values equal to ones from Xuan+ 22 (to visually inspect what the plot should look like)
-    m.results.post[:, m.results.system.param_idx['m1']] = (67.2 * u.M_jup / u.M_sun).to('').value
-    m.results.post[:, m.results.system.param_idx['sma1']] = 10.0
-    m.results.post[:, m.results.system.param_idx['ecc1']] = 0.7317
-    m.results.post[:, m.results.system.param_idx['inc1']] = np.radians(48.0)
-    m.results.post[:, m.results.system.param_idx['aop1']] = np.radians(267.2)
-    m.results.post[:, m.results.system.param_idx['pan1']] = np.radians(89.4)
-    m.results.post[:, m.results.system.param_idx['tau1']] = ((2462615 - my_driver.system.tau_ref_epoch) / (33.2 * 365.25)) % 1
-    m.results.post[:, m.results.system.param_idx['gamma_defrv']] = 0.0
-
 
     # Test plotting with single orbit
     _ = m.results.plot_orbits(
@@ -72,10 +59,6 @@ def test_rv_default_inst(save_plot=False):
         plot_astrometry_insts=True,
     )
 
-
-    if save_plot:
-        import matplotlib.pyplot as plt
-        plt.savefig('HD4747_orbit.png')
 
 
 def test_rv_multiple_inst():
@@ -252,7 +235,7 @@ def test_secondary_and_primary_rv():
 
 
 if __name__ == "__main__":
-    test_rv_default_inst(save_plot=True)
+    # test_rv_default_inst()
     # test_rv_multiple_inst()
-    # test_secondary_rv()
+    test_secondary_rv()
     # test_secondary_and_primary_rv()
