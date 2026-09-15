2a53557f9bbbe02e63865610541020d628b70611
Sarah Blunt <sarah.blunt.3@gmail.com>
2026-09-15 16:39:59 -0600

    change burn steps back to reasonable value

--- tests/end-to-end-tests/hd4747.py
+++ tests/end-to-end-tests/hd4747.py
@@ -73,14 +73,14 @@ my_driver.sampler.check_prior_support()
 assert len(my_driver.system.rv[0]) == 56
 
 total_orbits = 10_00_000
-burn_steps = 0#10_000
+burn_steps = 10_000
 thin = 10
 
 if __name__ == '__main__':
 
     # Run Sampler
     m = my_driver.sampler
-    m.run_sampler(total_orbits, burn_steps=burn_steps, thin=thin, periodic_save_freq=20, output_filename='foo.hdf5')
+    m.run_sampler(total_orbits, burn_steps=burn_steps, thin=thin, periodic_save_freq=1_000, output_filename='hd4747.hdf5')
     my_driver.sampler.results.save_results('hd4747_orbits.hdf5')
 
     my_results = results.Results()
